204590058aa172d0b82139f62de24d6579fbb9fc
zajzaj <1647880590@qq.com>
2014-10-26 10:45:42 +0800

    Add head

---
+++ Microblog/src/com/example/blog163/LoginInActivity.java
@@ -0,0 +1,15 @@
+package com.example.blog163;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LoginInActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
+}
